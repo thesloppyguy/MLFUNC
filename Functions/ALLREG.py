910ddcc4467fc6207bdbf036e331e752fca0850d
thesloppyguy <sahilmandal@outlook.com>
2021-09-18 20:40:52 +0530

    More Regression Models added

--- Functions/ALLREG.py
+++ Functions/ALLREG.py
@@ -5,6 +5,13 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.linear_model import PoissonRegressor
+#from sksurv.linear_model import CoxPHSurvivalAnalysis
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -55,12 +62,54 @@ def test_all_regressor(x_train, y_train, x_test, y_test):
     rf.fit(x_train, y_train)
     y_pred_rf = rf.predict(x_test)
 
+    # logistic regression
+    lr = LogisticRegression()
+    lr.fit(x_train, y_train)
+    y_pred_lr = lr.predict(x_test)
+
+    # rigid regression
+    rg = Ridge(alpha=1.0)
+    rg.fit(x_train, y_train)
+    y_pred_rg = rg.predict(x_test)
+
+    # lasso regression
+    las = Lasso(alpha=1.0)
+    las.fit(x_train, y_train)
+    y_pred_las = las.predict(x_test)
+
+    # elastic net regression
+    elnet = ElasticNet(random_state=0)
+    elnet.fit(x_train, y_train)
+    y_pred_elnet = elnet.predict(x_test)
+
+    # pls regression
+    pls2 = PLSRegression(n_components=2)
+    pls2.fit(x_train, y_train)
+    y_pred_pls2 = pls2.predict(x_test)
+
+    # poisson regression
+    ps = PoissonRegressor()
+    ps.fit(x_train, y_train)
+    y_pred_ps = ps.predict(x_test)
+
+    # cox regression
+    # cox = CoxPHSurvivalAnalysis()
+    # cox.fit(x_train, y_train)
+    # y_pred_cox = cox.predict(x_test)
+
     # r2 score
     print("multi :"+str(r2_score(y_test, y_pred_multi)))
     print("poly :"+str(r2_score(y_test, y_pred_poly)))
     print("support vector :"+str(r2_score(y_test, y_pred_sr)))
     print("decision tree :"+str(r2_score(y_test, y_pred_dt)))
     print("random forest :"+str(r2_score(y_test, y_pred_rf)))
+    print("logistic regression :"+str(r2_score(y_test, y_pred_lr)))
+    print("rigid regression :"+str(r2_score(y_test, y_pred_rg)))
+    print("lasso regression :"+str(r2_score(y_test, y_pred_las)))
+    print("elastic net regression :"+str(r2_score(y_test, y_pred_elnet)))
+    print("PLS regression :"+str(r2_score(y_test, y_pred_pls2)))
+    print("poisson regression :"+str(r2_score(y_test, y_pred_ps)))
+    #print("cox regression :"+str(r2_score(y_test, y_pred_cox)))
 
 
 def test_all_classifires():
